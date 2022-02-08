4da302963717f231cc95813e045290ca78a45191
penguin <grizzly@Macintosh.local>
2022-02-08 16:10:43 +0900

    HelloSample

--- HelloSample/app/src/main/java/com/example/hellosample/MainActivity.kt
+++ HelloSample/app/src/main/java/com/example/hellosample/MainActivity.kt
@@ -2,10 +2,28 @@ package com.example.hellosample
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val clickButton: Button = findViewById(R.id.clickButton)
+        val listener = HelloListener()
+        clickButton.setOnClickListener(listener)
+    }
+
+    private inner class HelloListener : View.OnClickListener{
+        override fun onClick(v: View?) {
+            val inputName: EditText = findViewById(R.id.nameEdit)
+            val outputText: TextView = findViewById(R.id.outputText)
+            val inputStr = inputName.text.toString()
+
+            outputText.text = inputStr + "さん、こんにちは！"
+        }
     }
 }
